9472cc91311248605aa83d2f7910598eed998bd2
jinzhiguang <jinzhiguang@kylinos.cn>
2024-11-26 11:25:52 +0800

    Ensure that there is only one window: message, help, version

--- kyrpm-installer/mainwindow.cpp
+++ kyrpm-installer/mainwindow.cpp
@@ -68,9 +68,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::showInfoMessage(bool)
 {
-    MessageDlg *msgDlg = new MessageDlg();
+    static MessageDlg *msgDlg = new MessageDlg();
     msgDlg->setOptions(m_resultList);
     msgDlg->show();
+    msgDlg->activateWindow();
 }
 
 void MainWindow::initSignals()
@@ -199,8 +200,9 @@ void MainWindow::showUI()
 
 void MainWindow::help(bool)
 {
-    HelpDlg *helpDlg = new HelpDlg();
+    static HelpDlg *helpDlg = new HelpDlg();
     helpDlg->show();
+    helpDlg->activateWindow();
 }
 
 void MainWindow::installEnd(QString result)
@@ -212,8 +214,9 @@ void MainWindow::installEnd(QString result)
 
 void MainWindow::displayVersion(bool)
 {
-    VersionDlg *verDlg = new VersionDlg();
+    static VersionDlg *verDlg = new VersionDlg();
     verDlg->show();
+    verDlg->activateWindow();
 }
 
 
